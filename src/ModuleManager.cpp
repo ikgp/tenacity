Paul-Licameli/audacity

ModuleManager and PluginManager don't use AudacityMessageBox

src/ModuleManager.cpp
@@ -38,8 +38,6 @@ i.e. an alternative to the usual interface, for Audacity.
 #include "ModuleSettings.h"
 #endif
 
-#include "widgets/AudacityMessageBox.h"
-
 #define initFnName      "ExtensionModuleInit"
 #define versionFnName   "GetVersionString"
 
@@ -57,16 +55,25 @@ Module::~Module()
 {
 }
 
+static BasicUI::MessageBoxResult DoMessageBox(const TranslatableString &msg)
+{
+   using namespace BasicUI;
+   return ShowMessageBox(msg,
+      MessageBoxOptions{}.Caption(XO("Module Unsuitable")));
+}
+
 void Module::ShowLoadFailureError(const wxString &Error)
 {
    auto ShortName = wxFileName(mName).GetName();
-   AudacityMessageBox(XO("Unable to load the \"%s\" module.\n\nError: %s").Format(ShortName, Error),
-                      XO("Module Unsuitable"));
+   DoMessageBox(
+      XO("Unable to load the \"%s\" module.\n\nError: %s")
+         .Format(ShortName, Error));
    wxLogMessage(wxT("Unable to load the module \"%s\". Error: %s"), mName, Error);
 }
 
 bool Module::Load(wxString &deferredErrorMessage)
 {
+   using namespace BasicUI;
    deferredErrorMessage.clear();
    // Will this ever happen???
    if (mLib->IsLoaded()) {
@@ -91,10 +98,9 @@ bool Module::Load(wxString &deferredErrorMessage)
    // Check version string matches.  (For now, they must match exactly)
    tVersionFn versionFn = (tVersionFn)(mLib->GetSymbol(wxT(versionFnName)));
    if (versionFn == NULL){
-      AudacityMessageBox(
+      ShowMessageBox(
          XO("The module \"%s\" does not provide a version string.\n\nIt will not be loaded.")
-            .Format( ShortName),
-         XO("Module Unsuitable"));
+            .Format( ShortName));
       wxLogMessage(wxT("The module \"%s\" does not provide a version string. It will not be loaded."), mName);
       mLib->Unload();
       return false;
@@ -102,10 +108,9 @@ bool Module::Load(wxString &deferredErrorMessage)
 
    wxString moduleVersion = versionFn();
    if( moduleVersion != AUDACITY_VERSION_STRING) {
-      AudacityMessageBox(
+      ShowMessageBox(
          XO("The module \"%s\" is matched with Audacity version \"%s\".\n\nIt will not be loaded.")
-            .Format(ShortName, moduleVersion),
-         XO("Module Unsuitable"));
+            .Format(ShortName, moduleVersion));
       wxLogMessage(wxT("The module \"%s\" is matched with Audacity version \"%s\". It will not be loaded."), mName, moduleVersion);
       mLib->Unload();
       return false;
@@ -126,9 +131,9 @@ bool Module::Load(wxString &deferredErrorMessage)
 
    mDispatch = NULL;
 
-   AudacityMessageBox(
-      XO("The module \"%s\" failed to initialize.\n\nIt will not be loaded.").Format(ShortName),
-      XO("Module Unsuitable"));
+   ShowMessageBox(
+      XO("The module \"%s\" failed to initialize.\n\nIt will not be loaded.")
+         .Format(ShortName));
    wxLogMessage(wxT("The module \"%s\" failed to initialize.\nIt will not be loaded."), mName);
    mLib->Unload();
 
@@ -241,6 +246,7 @@ void ModuleManager::FindModules(FilePaths &files)
 void ModuleManager::TryLoadModules(
    const FilePaths &files, FilePaths &decided, DelayedErrors &errors)
 {
+   using namespace BasicUI;
    FilePaths checked;
    wxString saveOldCWD = ::wxGetCwd();
    auto cleanup = finally([&]{ ::wxSetWorkingDirectory(saveOldCWD); });
@@ -321,9 +327,9 @@ void ModuleManager::TryLoadModules(
          if (!module->HasDispatch())
          {
             auto ShortName = wxFileName(file).GetName();
-            AudacityMessageBox(
-               XO("The module \"%s\" does not provide any of the required functions.\n\nIt will not be loaded.").Format(ShortName),
-               XO("Module Unsuitable"));
+            ShowMessageBox(
+               XO("The module \"%s\" does not provide any of the required functions.\n\nIt will not be loaded.")
+                  .Format(ShortName));
             wxLogMessage(wxT("The module \"%s\" does not provide any of the required functions. It will not be loaded."), file);
             module->Unload();
          }

ikgp/tenacity

===============================================================================
Cherry-picked from Audacity commit b01bc73. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ModuleManager.cpp
@@ -41,8 +41,6 @@ i.e. an alternative to the usual interface, for Audacity.
 #include "ModuleSettings.h"
 #endif
 
-#include "widgets/AudacityMessageBox.h"
-
 #define initFnName      "ExtensionModuleInit"
 #define versionFnName   "GetVersionString"
 
@@ -60,11 +58,19 @@ Module::~Module()
 {
 }
 
+static GenericUI::MessageBoxResult DoMessageBox(const TranslatableString &msg)
+{
+   using namespace GenericUI;
+   return ShowMessageBox(msg,
+      MessageBoxOptions{}.Caption(XO("Module Unsuitable")));
+}
+
 void Module::ShowLoadFailureError(const wxString &Error)
 {
    auto ShortName = wxFileName(mName).GetName();
-   AudacityMessageBox(XO("Unable to load the \"%s\" module.\n\nError: %s").Format(ShortName, Error),
-                      XO("Module Unsuitable"));
+   DoMessageBox(
+      XO("Unable to load the \"%s\" module.\n\nError: %s")
+         .Format(ShortName, Error));
    wxLogMessage(wxT("Unable to load the module \"%s\". Error: %s"), mName, Error);
 }
 
@@ -94,10 +100,9 @@ bool Module::Load(wxString &deferredErrorMessage)
    // Check version string matches.  (For now, they must match exactly)
    tVersionFn versionFn = (tVersionFn)(mLib->GetSymbol(wxT(versionFnName)));
    if (versionFn == NULL){
-      AudacityMessageBox(
+      DoMessageBox(
          XO("The module \"%s\" does not provide a version string.\n\nIt will not be loaded.")
-            .Format( ShortName),
-         XO("Module Unsuitable"));
+            .Format( ShortName));
       wxLogMessage(wxT("The module \"%s\" does not provide a version string. It will not be loaded."), mName);
       mLib->Unload();
       return false;
@@ -105,10 +110,9 @@ bool Module::Load(wxString &deferredErrorMessage)
 
    wxString moduleVersion = versionFn();
    if( moduleVersion != AUDACITY_VERSION_STRING) {
-      AudacityMessageBox(
+      DoMessageBox(
          XO("The module \"%s\" is matched with Audacity (or Tenacity) version \"%s\".\n\nIt will not be loaded.")
-            .Format(ShortName, moduleVersion),
-         XO("Module Unsuitable"));
+            .Format(ShortName, moduleVersion));
       wxLogMessage(wxT("The module \"%s\" is matched with Audacity (or Tenacity) version \"%s\". It will not be loaded."), mName, moduleVersion);
       mLib->Unload();
       return false;
@@ -129,9 +133,9 @@ bool Module::Load(wxString &deferredErrorMessage)
 
    mDispatch = NULL;
 
-   AudacityMessageBox(
-      XO("The module \"%s\" failed to initialize.\n\nIt will not be loaded.").Format(ShortName),
-      XO("Module Unsuitable"));
+   DoMessageBox(
+      XO("The module \"%s\" failed to initialize.\n\nIt will not be loaded.")
+         .Format(ShortName));
    wxLogMessage(wxT("The module \"%s\" failed to initialize.\nIt will not be loaded."), mName);
    mLib->Unload();
 
@@ -324,9 +328,9 @@ void ModuleManager::TryLoadModules(
          if (!module->HasDispatch())
          {
             auto ShortName = wxFileName(file).GetName();
-            AudacityMessageBox(
-               XO("The module \"%s\" does not provide any of the required functions.\n\nIt will not be loaded.").Format(ShortName),
-               XO("Module Unsuitable"));
+            DoMessageBox(
+               XO("The module \"%s\" does not provide any of the required functions.\n\nIt will not be loaded.")
+                  .Format(ShortName));
             wxLogMessage(wxT("The module \"%s\" does not provide any of the required functions. It will not be loaded."), file);
             module->Unload();
          }